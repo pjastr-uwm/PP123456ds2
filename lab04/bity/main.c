999cc7bebdeda43d9e312e87e2a1352bf5da242d
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-03-04 12:55:39 +0100

    4_2

---
+++ lab04/bity/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a =11, b=6;
+    printf("a&b= %d\n", a&b);
+    printf("a|b= %d\n", a|b);
+    printf("a^b= %d\n", a^b);
+    printf("a<<1= %d\n", a<<1);
+    printf("a<<2= %d\n", a<<2);
+    printf("a>>1= %d\n", a>>1);
+    printf("a>>2= %d\n", a>>2);
+    printf("~a= %d\n", ~a);
+    return 0;
+}
