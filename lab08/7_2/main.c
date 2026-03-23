09ad50745aee14cd13cf67504b53b5c712acf2eb
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-03-23 13:33:06 +0100

    7_7

--- lab08/7_2/main.c
+++ lab08/7_2/main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 float calculateAbsoluteValue(float a){
-    if (a>0)
+    if (a>=0)
         return a;
     return -a;
 }
 
 int main()
 {
-    printf("%f\n", calculateAbsoluteValue(5));
-    printf("%f\n", calculateAbsoluteValue(-12));
+    printf("%f\n", calculateAbsoluteValue(5.8));
+    printf("%f\n", calculateAbsoluteValue(-12.7));
     printf("%f\n", calculateAbsoluteValue(0));
     return 0;
 }
---
+++ lab09/7_7/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculateSquareRootFloor(int n){
+    int i=0;
+    while(i*i<=n){
+        i++;
+    }
+    return i-1;
+}
+
+int main()
+{
+    printf("%d\n", calculateSquareRootFloor(0));
+    printf("%d\n", calculateSquareRootFloor(1));
+    printf("%d\n", calculateSquareRootFloor(2));
+    printf("%d\n", calculateSquareRootFloor(3));
+    printf("%d\n", calculateSquareRootFloor(4));
+
+    printf("%d\n", calculateSquareRootFloor(35));
+    printf("%d\n", calculateSquareRootFloor(36));
+    printf("%d\n", calculateSquareRootFloor(37));
+    return 0;
+}
