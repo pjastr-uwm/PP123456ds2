96af80f4625f178411cd9f220e70365bda7af157
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-03-30 13:41:08 +0200

    8_9

---
+++ lab11/8_9/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sqrCopy(int const * p1, int * p2){
+    *p2 = *p1 * *p1;
+}
+
+int main()
+{
+    int a =5, b =17;
+    printf("%d %d\n", a,b);
+    sqrCopy(&a, &b);
+    printf("%d %d\n", a,b);
+    return 0;
+}
