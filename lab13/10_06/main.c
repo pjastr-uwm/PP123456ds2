b6af0da7274a5c749f5feb1959e34f2b6e6e849d
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-04-13 14:40:30 +0200

    10_06

--- lab13/10_06/main.c
+++ lab13/10_06/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void reverseArr(int n, int tab[]){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n/2;i++){
         int temp = tab[i];
         tab[i] = tab[n-1-i];
         tab[n-1-i] = temp;
