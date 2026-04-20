d7655d9086e62451e11bcc1b31e8e5ce6a2c9b3d
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-04-20 13:55:35 +0200

    11_1

--- lab15/11_1/main.c
+++ lab15/11_1/main.c
@@ -33,11 +33,29 @@ int length4(char * txt){
     return i;
 }
 
+int length5(char * txt){
+    int i=0;
+    while(*(txt+i)){
+        i++;
+    }
+    return i;
+}
+
+int length6(char * txt){
+    if (txt[0] == 0){
+        return 0;
+    }
+    return 1+length6(++txt);
+}
+
+
 int main()
 {
     printf("%d\n", length("Olsztyn"));
     printf("%d\n", length2("Olsztyn"));
     printf("%d\n", length3("Olsztyn"));
     printf("%d\n", length4("Olsztyn"));
+    printf("%d\n", length5("Olsztyn"));
+    printf("%d\n", length6(""));
     return 0;
 }
