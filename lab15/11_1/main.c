06ef4dc821f7a4f35b415d4a09ca15ca83db67a2
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-04-20 13:41:27 +0200

    11_1

---
+++ lab15/11_1/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int length(char * txt){
+    int i=0;
+    while(txt[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
+int length2(char * txt){
+    int i;
+    for(i=0; txt[i] != '\0'; i++){
+
+    }
+    return i;
+}
+
+int length3(char * txt){
+    int i=0;
+    while(txt[i] != 0){
+        i++;
+    }
+    return i;
+}
+
+int length4(char * txt){
+    int i=0;
+    while(txt[i]){
+        i++;
+    }
+    return i;
+}
+
+int main()
+{
+    printf("%d\n", length("Olsztyn"));
+    printf("%d\n", length2("Olsztyn"));
+    printf("%d\n", length3("Olsztyn"));
+    printf("%d\n", length4("Olsztyn"));
+    return 0;
+}
