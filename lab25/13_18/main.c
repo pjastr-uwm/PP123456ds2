b1540c888f568dd440b907bbb47b2048083bfd7b
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-05-20 12:01:42 +0200

    13_18

---
+++ lab25/13_18/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum Day { MON, TUE, WED, THU, FRI, SAT, SUN};
+
+void print_days(enum Day day, int n){
+    if(n<=0)
+        return;
+    switch(day){
+        case MON:
+            printf("MON");
+            break;
+        case TUE:
+            printf("TUE");
+            break;
+        case WED:
+            printf("WED");
+            break;
+        case THU:
+            printf("THU");
+            break;
+        case FRI:
+            printf("FRI");
+            break;
+        case SAT:
+            printf("SAT");
+            break;
+        case SUN:
+            printf("SUN");
+            break;
+    }
+    printf("\n");
+    day = (day+1)%7;
+    print_days(day, --n);
+}
+
+int main()
+{
+    enum Day d1 = WED;
+    print_days(d1,10);
+    return 0;
+}
