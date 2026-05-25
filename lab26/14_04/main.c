9460471d01bacae8c951ded092032f4059b19c2a
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2026-05-25 14:33:28 +0200

    14_04

--- lab26/14_04/main.c
+++ lab26/14_04/main.c
@@ -6,8 +6,8 @@ struct element {
     struct element * next;
 };
 
-void printListWithoutHead(struct element * list){
-    struct element * ptr = list;
+void printListWithHead(struct element * list){
+    struct element * ptr = list->next;
     if (ptr == NULL){
         printf("Empty list\n---\n");
         return;
@@ -22,14 +22,16 @@ void printListWithoutHead(struct element * list){
 int main()
 {
     struct element * list = malloc(sizeof(struct element));
-    list->x = 4;
     list->next = malloc(sizeof(struct element));
-    list->next->x = 5;
+    list->next->x = 4;
     list->next->next = malloc(sizeof(struct element));
-    list->next->next->x = -12;
-    list->next->next->next = NULL;
-    printListWithoutHead(list);
-    struct element * list2 = NULL;
-    printListWithoutHead(list2);
+    list->next->next->x = 5;
+    list->next->next->next = malloc(sizeof(struct element));
+    list->next->next->next->x = -12;
+    list->next->next->next->next = NULL;
+    printListWithHead(list);
+    struct element * list2 = malloc(sizeof(struct element));
+    list2->next = NULL;
+    printListWithHead(list2);
     return 0;
 }
